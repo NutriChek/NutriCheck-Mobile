4cf03a3c3be5d39a3637207fbdfc4675d07f552b
Victoria-Elena Borcea <victoriaborcea29@gmail.com>
2024-08-12 12:09:22 +0300

    Added svg background

--- app/(app)/(home)/_layout.tsx
+++ app/(app)/(home)/_layout.tsx
@@ -4,35 +4,37 @@ import TabBar from '@/components/tab-bar';
 
 export default function Layout() {
   return (
-    <Tabs tabBar={(props) => <TabBar {...props} />}>
-      <Tabs.Screen
-        name='index'
-        options={{
-          headerTransparent: true,
-          header: () => <HomeHeader text='Good morning, Alex' />
-        }}
-      />
-      <Tabs.Screen
-        name='ai'
-        options={{
-          headerTransparent: true,
-          header: () => <HomeHeader text='Cook AI' />
-        }}
-      />
-      <Tabs.Screen
-        name='recipes'
-        options={{
-          headerTransparent: true,
-          header: () => <HomeHeader text='Recipes' />
-        }}
-      />
-      <Tabs.Screen
-        name='goals'
-        options={{
-          headerTransparent: true,
-          header: () => <HomeHeader text='Your goals' />
-        }}
-      />
-    </Tabs>
+    <>        
+      <Tabs tabBar={(props) => <TabBar {...props} />}>
+        <Tabs.Screen
+          name='index'
+          options={{
+            headerTransparent: true,
+            header: () => <HomeHeader text='Good morning, Alex' />
+          }}
+        />
+        <Tabs.Screen
+          name='ai'
+          options={{
+            headerTransparent: true,
+            header: () => <HomeHeader text='Cook AI' />
+          }}
+        />
+        <Tabs.Screen
+          name='recipes'
+          options={{
+            headerTransparent: true,
+            header: () => <HomeHeader text='Recipes' />
+          }}
+        />
+        <Tabs.Screen
+          name='goals'
+          options={{
+            headerTransparent: true,
+            header: () => <HomeHeader text='Your goals' />
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
